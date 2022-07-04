72b6288426440d6085479732c0814c6763774c0b
DeemoHarlos <cces4415@gmail.com>
2022-07-04 20:01:28 +0800

    fix bug

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #index
-    #content.position-relative(ref="bgRef")
+    #content.position-relative(ref="bgRef" :style="{ height: bgHeight + 'px' }")
       .moon(v-for="moonCss in moonsCss" :style="moonCss")
     #footer
 </template>
@@ -12,19 +12,20 @@ export default defineComponent({
   setup() {
     const bgRef = ref<(HTMLElement) | null>(null)
 
-    const moonSize = 0.2
+    const moonSize = 0.203
     const moonLeft = 0.187 - moonSize / 2
     const moonRight = 0.197 - moonSize / 2
     const bgWidth = ref(1920)
+    const bgHeight = computed(() => bgWidth.value * 2.59)
     const moonSizeCss = computed(() => (
       { width:  moonSize * bgWidth.value + 'px', height: moonSize * bgWidth.value + 'px' }
     ))
     const moonsCss = computed(() => [
-      { left:  moonLeft  * bgWidth.value + 'px', top: 0.843 * bgWidth.value + 'px', ...moonSizeCss },
-      { right: moonRight * bgWidth.value + 'px', top: 1.253 * bgWidth.value + 'px', ...moonSizeCss },
-      { left:  moonLeft  * bgWidth.value + 'px', top: 1.614 * bgWidth.value + 'px', ...moonSizeCss },
-      { right: moonRight * bgWidth.value + 'px', top: 1.968 * bgWidth.value + 'px', ...moonSizeCss },
-      { left:  moonLeft  * bgWidth.value + 'px', top: 2.310 * bgWidth.value + 'px', ...moonSizeCss },
+      { left:  moonLeft  * bgWidth.value + 'px', top: 0.843 * bgWidth.value + 'px', ...moonSizeCss.value },
+      { right: moonRight * bgWidth.value + 'px', top: 1.253 * bgWidth.value + 'px', ...moonSizeCss.value },
+      { left:  moonLeft  * bgWidth.value + 'px', top: 1.614 * bgWidth.value + 'px', ...moonSizeCss.value },
+      { right: moonRight * bgWidth.value + 'px', top: 1.968 * bgWidth.value + 'px', ...moonSizeCss.value },
+      { left:  moonLeft  * bgWidth.value + 'px', top: 2.310 * bgWidth.value + 'px', ...moonSizeCss.value },
     ])
 
     onMounted(() => {
@@ -37,6 +38,7 @@ export default defineComponent({
     return {
       bgRef,
       bgWidth,
+      bgHeight,
       moonsCss,
     }
   },
@@ -49,30 +51,11 @@ $moon-left: calc(18.6vw - #{$moon-size} / 2)
 $moon-right: calc(20vw - #{$moon-size} / 2)
 
 #content
-  width: 100%
-  height: 259vw
   background: no-repeat top/100% url('~assets/images/bg2.png')
   .moon
     background-color: #EEF09B
     position: absolute
-    width: $moon-size
-    height: $moon-size
     border-radius: 50%
-    &:nth-child(1)
-      left: $moon-left
-      top: 83.1vw
-    &:nth-child(2)
-      right: $moon-right
-      top: 123.8vw
-    &:nth-child(3)
-      left: $moon-left
-      top: 159.5vw
-    &:nth-child(4)
-      right: $moon-right
-      top: 194.4vw
-    &:nth-child(5)
-      left: $moon-left
-      top: 228vw
 #footer
   background-color: #85BDF7
   height: 10vw
