228cac013d4086dc2a27d054418c392e5144cea0
DeemoHarlos <cces4415@gmail.com>
2022-07-14 09:05:29 +0800

    tarot page

---
+++ pages/project.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  #project
+    #navbar.pt-5.pb-4
+      .flex-row.middle-center
+        NuxtLink.nav-button.px-4(to="/") HOME
+        NuxtLink.nav-button.px-4(to="/project/tarot") TAROT
+        NuxtLink.nav-button.px-4(to="/project/video") VIDEO
+        NuxtLink.nav-button.px-4(to="/project/comment") COMMENT
+        NuxtLink.nav-button.px-4(to="/project/staff") STAFF
+        .flex-fill
+        #profile-pic.px-4
+          img(src="~assets/images/profile.png")
+    NuxtChild
+    #footer
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+#project
+  background-color: #F1FFFF
+  #navbar
+    color: #737373
+    &>div
+      height: 120px
+      .nav-button
+        text-decoration: none
+        color: inherit
+        font-size: 1.5em
+        font-weight: 500
+        &::first-letter
+          font-size: 1.5em
+      #profile-pic
+        img
+          width: 120px
+  #footer
+    background-color: #122D42
+    min-height: 100px
+</style>
---
+++ pages/project/tarot.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  #tarot
+    #banner.flex-column.middle-center
+      p.title TAROT PROJECT
+      p タロットプロジェックト / 塔羅牌企劃
+    #content.p-5
+      #tab-buttons.flex-row.middle-center
+        .btn.button.text-center.py-2.mr-3 GAME
+        .btn.button.text-center.py-2 ARTISTS
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+#tarot
+  min-height: 100vh
+  #banner
+    background-color: #CBA1F5
+    color: #FFFFFF
+    height: 300px
+    .title
+      font-size: 2.5em
+      margin: 50px 0 .25em
+  #content
+    color: #5D5D5D
+    #tab-buttons
+      .button
+        font-size: 2em
+        color: #5D5D5D
+        border: #5D5D5D solid 3px
+        width: 250px
+        border-radius: 25px
+        &:hover
+          background-color: #5D5D5D
+          color: #FFFFFF
+</style>
